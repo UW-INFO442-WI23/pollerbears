2fd1bc4833acdd77122f3a33121e9ab0eb5ec0e7
Your Name <you@example.com>
2023-02-01 11:04:48 -0800

    react router stuff

---
+++ src/App.js
@@ -0,0 +1,15 @@
+/* React Router goes here */
+import navbar from "src\components\Nav.js"
+import homescreen from "src\components\Home.js"
+import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom"
+export default function App(props){
+    return(
+        <div>
+            <Router>
+                <Routes>
+
+                </Routes>
+            </Router>
+        </div>
+    );
+}
