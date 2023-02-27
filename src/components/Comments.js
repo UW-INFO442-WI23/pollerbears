c478dfd8ad451ddb41347e788f98baba661a20dd
Ivers89 <ili88@uw.edu>
2023-02-27 15:15:45 -0800

    Adding comments to page, Bugs: Comments aren't unique to each page, Comments duplicate when typing anything in the textarea

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,15 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import firebase from '../index'
 
 const Comments = () => {
+
+  const ref = firebase.firestore().collection("Comments")
+  const [data, setdata] = useState([])
+  const [loader, setloader] = useState(true)
+  
+  function getData(){
+  
+    ref.onSnapshot(querySnapshot => {
+      const comments = []
+      querySnapshot.forEach((doc) => {
+        comments.push(doc.data())
+      })
+      setdata(comments)
+      setloader(false)
+    })
+  }
+  
+  useEffect(() => {
+    getData()
+  },[])
+
   const [commentList, setCommentList] = useState([
+    
     { username: 'John', profilePic: 'https://via.placeholder.com/50x50', comment: 'Absolutely in support of this bill!' },
     { username: 'Jane', profilePic: 'https://via.placeholder.com/50x50', comment: 'I\'m kind of hesitant about this one, but I\'ll have to do more research.' },
     { username: 'Alex', profilePic: 'https://via.placeholder.com/50x50', comment: 'Voting yes immediately.' },
+
   ]);
 
-  const [newComment, setNewComment] = useState('');
+  loader === false && (data.map((newComments) => ( 
+    commentList.push({
+      username: newComments.username,
+      profilePic: 'https://via.placeholder.com/50x50',
+      comment: newComments.newComment,
+    })
+  )))
 
-  const handleAddComment = () => {
+  const [newComment, setNewComment] = useState('');
+  const [username, setusername] = useState("new User")
+
+  function refreshComments() {
+    loader === false && (data.map((newComments) => ( 
+      commentList.push({
+        username: newComments.username,
+        profilePic: 'https://via.placeholder.com/50x50',
+        comment: newComments.newComment,
+      })
+    )))
+  }
+
+  function addComment(newDataObj){
+    console.log("200")
+    ref.doc().set(newDataObj).catch((err) => {
+      alert(err)
+      console.error(err)
+    })
+    setNewComment('')
+  }
+
+  /*const handleAddComment = () => {
     if (newComment.trim() !== '') {
       const newCommentList = [...commentList];
       newCommentList.push({
@@ -20,10 +72,10 @@ const Comments = () => {
       setCommentList(newCommentList);
       setNewComment('');
     }
-  };
+  };*/
 
   return (
-    <div className="comment-section">
+    <div  className="comment-section">
       <div className="comment-input-container">
         <textarea
           type="text"
@@ -31,7 +83,10 @@ const Comments = () => {
           value={newComment}
           onChange={(e) => setNewComment(e.target.value)}
         />
-        <button onClick={handleAddComment}>Post</button>
+        <button onClick={() => {
+          setusername('new User')
+          addComment({newComment, username})
+        }}>Post</button>
       </div>
       <div className="comment-list">
         {commentList.map((comment, index) => (
